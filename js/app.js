1a82ecfb717d42216ac923e28bd370f2aa105269
Enrique Git <97200594+enriquegraficos@users.noreply.github.com>
2022-09-22 11:18:32 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody')
+const listaCursos = document.querySelector('#lista-cursos');
